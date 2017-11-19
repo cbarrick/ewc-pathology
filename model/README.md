c935aaff0bee7f31556d0b0e4082955d8c145815
Aditya Shinde <shinde.aditya386@gmail.com>
2017-11-19 11:08:05 -0500

    EWC works bitchesgit status! \m/

--- model/README.md
+++ model/README.md
@@ -44,3 +44,31 @@ for i in range(EPOCHS):
     train_x=numpy.transpose(train_x,(0,3,1,2))
     model.partial_fit(train_x,train_y)
 ```
+
+## Updates
+
+### EWC now works!
+
+The AlexNet model now has a `consolidate(self,x,y)` method for consolidating weights after training on a task.
+
+Now the computation of the Fisher needs samples from the dataset. So we will have to store a few samples from every task during training. and then call `consolidate(taskA_sample_x,taskA_sample_y)` to consolidate the weights for task A before starting training on task B.
+
+If have written a small demo network (`testnet.py`) and a script (`demo.py`) to demonstrate the effect of training with and without EWC.
+
+You'll need an up to date version of keras to run the demo cos I am using keras datasets. So just run `conda install -c conda_forge keras` to update.
+
+To train without EWC, run
+`python3 demo.py -e 50`
+And notice how accuracy of task A drops significantly after training for task B
+
+Now run, `python3 demo.py -e 50 --ewc`
+And now compare the accuracy of task A at the end. 
+
+When we use this, we will have to call `consolidate(self,x,y)` after every task so that the network prepares the fishers for EWC loss for the next task.
+Like this
+```python
+model=testnet.AlexNet(10)
+mnist_data=train_on_task("MNIST",batchify(mnist_train_x,mnist_train_y))
+model.consolidate(numpy.transpose(taskA_sample_x,taskA_sample_y))
+fashion_data=train_on_task("FASHION",batchify(fashion_train_x,fashion_train_y))
+``` 
--- model/testnet.py
+++ model/testnet.py
@@ -5,6 +5,7 @@ import torch.optim as O
 import torch.autograd as A
 import torchvision.models as Models
 import logging
+import numpy
 
 logger=logging.getLogger(__name__)
 
@@ -16,28 +17,18 @@ class AlexNet(N.Module):
         self.mean={}
         self.__CONSOLIDATED=0
         self.features = N.Sequential(
-            N.Conv2d(3, 64, kernel_size=11, stride=4, padding=1),
+            N.Conv2d(1, 3, kernel_size=3, stride=1, padding=1),
             N.ReLU(inplace=True),
-            N.MaxPool2d(kernel_size=3, stride=2),
-            N.Conv2d(64, 192, kernel_size=5, padding=1),
+            #N.MaxPool2d(kernel_size=3, stride=2)
+            N.Conv2d(3,3,kernel_size=5,stride=1,padding=2),
             N.ReLU(inplace=True),
-            N.MaxPool2d(kernel_size=3, stride=2),
-            N.Conv2d(192, 384, kernel_size=3, padding=1),
-            N.ReLU(inplace=True),
-            N.Conv2d(384, 256, kernel_size=3, padding=1),
-            N.ReLU(inplace=True),
-            N.Conv2d(256, 256, kernel_size=3, padding=1),
-            N.ReLU(inplace=True),
-            N.MaxPool2d(kernel_size=3, stride=2),
+            N.MaxPool2d(kernel_size=3,stride=2)
         )
         self.classifier = N.Sequential(
             N.Dropout(),
-            N.Linear(256 * 6 * 6, 4096),
+            N.Linear(3*13*13, 100),
             N.ReLU(inplace=True),
-            N.Dropout(),
-            N.Linear(4096, 4096),
-            N.ReLU(inplace=True),
-            N.Linear(4096, num_classes),
+            N.Linear(100, num_classes)
         )
         logger.debug("CNN Initialised.")
         self.lossfn=N.CrossEntropyLoss()
@@ -46,13 +37,18 @@ class AlexNet(N.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
+        x = x.view(x.size(0), 3*13*13)
         x = self.classifier(x)
         return x
 
     def partial_fit(self,X,y):
-        out=self.forward(X)
-        loss=self.lossfn(out,y)
+        X=A.Variable(torch.from_numpy(X))
+        y=A.Variable(torch.from_numpy(y))
+        out=self.forward(X.float())
+        if self.__CONSOLIDATED:
+            loss=self.lossfn(out,y.long())+self.get_ewc_loss()
+        else:
+            loss=self.lossfn(out,y.long())
         logger.info("Loss: {}".format(loss.data[0]))
         self.optimizer.zero_grad()
         loss.backward()
@@ -60,7 +56,7 @@ class AlexNet(N.Module):
         return loss.data[0]
 
     def predict(self,x):
-        out=F.softmax(self.forward(x))
+        out=F.softmax(self.forward(A.Variable(torch.from_numpy(x)).float()))
         return torch.max(out.data,1)[1].numpy()
 
     def get_ewc_loss(self,l=15):
@@ -74,11 +70,14 @@ class AlexNet(N.Module):
 
     def consolidate(self,x,y):
         logger.debug("Consolidating weights.")
-        size=x.size()[0]
+        size=x.shape[0]
+        x=A.Variable(torch.from_numpy(x)).float()
+        y=A.Variable(torch.from_numpy(y)).long()
         log_liklihood=F.log_softmax(self.forward(x))[range(size),y.data]
         derivatives=A.grad(log_liklihood.sum(),self.parameters())
         for i,(name,param) in enumerate(self.named_parameters()):
             self.fisher[name]=(derivatives[i].data.clone()**2)/size
             self.mean[name]=param.data.clone()
         self.__CONSOLIDATED=1
- 
+        
+        
