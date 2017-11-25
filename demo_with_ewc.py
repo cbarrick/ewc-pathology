a320601d16634287afc645423c6e37c6436a45a0
Prajay Shetty <prajayshetty@Prajays-MacBook-Air.local>
2017-11-25 14:17:38 -0500

    Demo files showing ewc support

--- demo_with_ewc.py
+++ demo_with_ewc.py
@@ -1,73 +1,83 @@
+# Entry point for EWC Trainer
 import argparse
-#sys.path.append("/home/adityas/Projects/DataScience2/model/")
-
 from model import  alexnet
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-from keras.datasets import mnist
 import numpy
-from scores_individual import Score
+from ewctrainer.Score import Score
 
 from datasets.nuclei import NucleiLoader
-from datasets.epi import EpitheliumLoader 
-from EWCTrainer import EWCTrainer
+from datasets.epi import EpitheliumLoader
+from ewctrainer.Trainer import Trainer
+
+import logging
+
 logger=logging.getLogger(__name__)
 
-parser=argparse.ArgumentParser()
-parser.add_argument("-e",type=int)
-args=parser.parse_args()
-print(args)
-EPOCHS=args.e
+"""
+Main function for calling the EWC Trainer
+
+"""
+if __name__ == "__main__":
+	parser=argparse.ArgumentParser()
+	parser.add_argument("-e", type = int, help = "epochs for the model")
+	args=parser.parse_args()
+	print(args)
+	EPOCHS = args.e
+
+	#code for loading training and testing datase for tasks 1 and 2
+
+	data_nuclei = NucleiLoader()
+	#data_epi= EpitheliumLoader()
+	trainer = Trainer(2)
+
+	#place holder for storing information regarding to prediction of task 1 and 2
+	y_pred_task_1 = list();
+	y_orig_task_1 = list();
+	y_pred_task_2 = list();
+	y_orig_task_2 = list();
 
 
-data_nuclei = NucleiLoader()
-data_epi= EpitheliumLoader()
-trainer = EWCTrainer(2)
+
+	print("----------------------Now trianing with ewc and getting metrics ---------------------------------------->")
+	#launch trianer for 2 classes and 1 epoch per batch
+
+	for i in range(0,EPOCHS):
+		logger.info("EPOCHS on EPI loader training { }".format(EPOCHS))
+		for x, y in data_nuclei.load_train(0,batch_size=100):
+			trainer.train_on_task("Task1 -AD HOC TASK",x ,y)
+			sample_data = x.clone(), y.clone()
+			break;
+
+	trainer.train_on_task_consolidate("CONSLIDATE WEIGHTS", sample_data[0], sample_data[1])
+
+	for i in range(0,EPOCHS):
+		logger.info("EPOCHS oN Nuclei loader training { }".format(EPOCHS))
+		for x, y in data_nuclei.load_train(0, batch_size=100):
+			print(x.shape)
+			print(y.shape)
+			sample_data = x.clone(),y.clone()
+			trainer.train_on_task("TASK-MAIN EWC TESTING TASK", x, y)
+			break;
 
 
-y_pred = list();
-y_orig = list();
 
-    
+	print("recording prediction of EWC Testing task")
 
-print("----------------------Now trianing with ewc and getting metrics ---------------------------------------->")
-trainer = EWCTrainer(2)
+	for x,y in data_nuclei.load_test(0, batch_size = 100):
+		y_pred_task_1.extend(trainer.predict(x))
+		y_orig_task_1.extend(y)
+		break;
 
-
-for i in range(0,EPOCHS):
-    for x, y in data_epi.load_train(0,batch_size=100):
-        trainer.train_on_task("Task1 -AD HOC TASK",x,y)
-        sample_data = x.clone(),y.clone()
-
-        
-
-trainer.train_on_task_consolidate("CONSLIDATE WEIGHTS",sample_data[0],sample_data[1])
-
-for i in range(0,EPOCHS):
-    for x, y in data_nuclei.load_train(0,batch_size=100):
-        print("on main  funcition")
-        print(x.shape)
-        print(y.shape)
-        sample_data = x.clone(),y.clone()
-        trainer.train_on_task("TASK-MAIN EWC TESTING TASK",x.clone(),y.clone())
- 
-        
-
-print("recording prediction of EWC Testing task")
-
-for x,y in data_nuclei.load_test(0,batch_size=100):
-    y_pred.extend(trainer.predict(x.clone()))
-    y_orig.extend(y)
-
-    
-
-s = Score();
-print("<---------------------------SCORE METRICS ----------------------------------------------------->")
-print(s.build_table(y_orig,y_pred));
-s.plot_roc(y_orig,y_pred)
-s.plot_loss(y_orig,y_pred)
-
-print("--------------------------SCORE METRIC END ----------------")
+	for x,y in data_nuclei.load_test(0, batch_size = 100):
+		y_pred_task_2.extend(trainer.predict(x))
+		y_orig_task_2.extend(y)
+		break;
 
 
+
+	s = Score();
+	print("<---------------------------SCORE METRICS ----------------------------------------------------->")
+	print(s.build_table(y_orig_task_1, y_orig_task_2, y_pred_task_1, y_pred_task_2));
+	s.plot_roc(y_orig_1, y_orig_2, y_pred_task_1, y_pred_task_2)
+	#s.plot_loss(y_orig,y_pred)
+
+	print("--------------------------SCORE METRIC END ----------------")
