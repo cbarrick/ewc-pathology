435119ebed3eb28b6ca5fbd5b7c5ae9a99540fe1
Chris Barrick <cbarrick1@gmail.com>
2017-11-25 18:40:18 -0500

    Make AlexNet match the network we're citing (caffe's CIFAR-10 Alex net)

--- models/alexnet.py
+++ models/alexnet.py
@@ -9,34 +9,29 @@ logger=logging.getLogger(__name__)
 
 class AlexNet(N.Module):
 
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=2):
         super(AlexNet, self).__init__()
         self.fisher={}
         self.mean={}
         self.__CONSOLIDATED=0
         self.features = N.Sequential(
-            N.Conv2d(3, 64, kernel_size=11, stride=4, padding=1),
+            N.Conv2d(3, 32, kernel_size=5, stride=1, padding=2),
+            N.MaxPool2d(kernel_size=3, stride=2, padding=1),
             N.ReLU(inplace=True),
-            #N.MaxPool2d(kernel_size=3, stride=2),
-            N.Conv2d(64, 192, kernel_size=5, padding=1),
+            N.Conv2d(32, 32, kernel_size=5, stride=1, padding=2),
             N.ReLU(inplace=True),
-            #N.MaxPool2d(kernel_size=3, stride=2),
-            N.Conv2d(192, 384, kernel_size=3, padding=1),
+            N.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            N.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
             N.ReLU(inplace=True),
-            N.Conv2d(384, 256, kernel_size=3, padding=1),
-            N.ReLU(inplace=True),
-            N.Conv2d(256, 256, kernel_size=3, padding=1),
-            N.ReLU(inplace=True),
-            N.MaxPool2d(kernel_size=3, stride=2),
+            N.AvgPool2d(kernel_size=3, stride=2, padding=1),
         )
         self.classifier = N.Sequential(
+            N.Linear(64*8*8, 64),
             N.Dropout(),
-            N.Linear(256 * 5 * 5, 4096),
             N.ReLU(inplace=True),
+            N.Linear(64, self.num_classes),
             N.Dropout(),
-            N.Linear(4096, 4096),
             N.ReLU(inplace=True),
-            N.Linear(4096, num_classes),
         )
         logger.debug("CNN Initialised.")
         self.lossfn=N.CrossEntropyLoss()
