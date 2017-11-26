cefbd9c4cf6f604b011ad7d67aa0a92fdac1ce45
Chris Barrick <cbarrick1@gmail.com>
2017-11-26 03:06:39 -0500

    Improve Trainer

--- train/trainer.py
+++ train/trainer.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import torch
 import torch.autograd as A
@@ -55,34 +56,36 @@ class EWCTrainer:
         h = self.model(x)
         l = F.log_softmax(h)[:, y]  # log-likelihood of true class
         l.backward()
-        grads = (p.grad.detach().data for p in self.params())
+        grads = (p.grad.data for p in self.params())
         fisher = [(g ** 2).mean(0) for g in grads]
         return fisher
 
-    def consolidate(self, x, y, alpha=1):
-        '''Consolidate the weights of the current task.
+    def consolidate(self, data, alpha=1):
+        '''Consolidate the weights given a sample of the current task.
 
         This adds an EWC regularization term to the loss function.
-
-        Args:
-            x: A sample of inputs for the task.
-            y: The associated labels.
-            alpha: The regularization constant for the new EWC term.
         '''
-        params = [p.detach().data for p in self.params()]
-        fisher = self.fisher_information(x, y)
+        params = [p.data.copy() for p in self.params()]
+        fisher = [torch.zeroes_like(p) for p in self.params()]
+
+        n = len(data)
+        for x, y in data:
+            for i, f in enumerate(self.fisher_information(x, y)):
+                fisher[i] += f / n
+
         task = {
             'params': params,
             'fisher': fisher,
             'alpha': alpha,  # The name 'lambda' is taken by the keyword.
         }
+
         self._tasks.append(task)
 
     def loss(self, h, y):
-        '''Compute the loss with EWC regularization.
+        '''Compute the loss between hypotheses and true label.
 
         Args:
-            h: The hypothesis.
+            h: The hypotheses.
             y: The labels.
         '''
         j = self._loss(h, y)
@@ -100,17 +103,6 @@ class EWCTrainer:
             j += sum(ewc)
         return j
 
-    def predict(self, x):
-        '''Evaluate the model on some input batch.
-
-        Args:
-            x: The input batch.
-        '''
-        self.model.eval()  # put the model in eval mode, effects dropout layers etc.
-        x = A.Variable(x, volatile=True)  # use volatile input to save memory when not training.
-        h = self.model(x)
-        return h
-
     def partial_fit(self, x, y):
         '''Performs one step of the optimization.
 
@@ -126,4 +118,48 @@ class EWCTrainer:
         j = self.loss(h, y)
         j.backward()
         self.opt.step()
-        return j
+        return j.data
+
+    def fit(self, data, max_epochs=int(1e3), out=sys.stdout):
+        '''Fit the model to a task.
+        '''
+        n = len(data)
+        for epoch in range(max_epochs):
+            print(f'epoch {epoch+1} ', end='', flush=True, file=out)
+            loss = 0
+            for x, y in data:
+                j = self.partial_fit(x, y)
+                loss += j.sum() / n
+                print('.', end='', flush=True, file=out)
+            print(f'[Train loss: {loss}]', file=out)
+        return loss
+
+    def predict(self, x):
+        '''Evaluate the model on some input batch.
+
+        Args:
+            x: The input batch.
+        '''
+        self.model.eval()  # put the model in eval mode, effects dropout layers etc.
+        x = A.Variable(x, volatile=True)  # use volatile input to save memory when not training.
+        h = self.model(x)
+        return h
+
+    def score(self, x, y, criteria=None):
+        '''Score the model against some criteria.
+
+        If the criteria is None, the loss is returned.
+        '''
+        h = self.predict(x)
+        j = self.loss(h, y) if criteris is None else criteria(h, y)
+        return j.data
+
+    def test(self, data, criteria=None):
+        '''Test the model against some task.
+        '''
+        n = len(data)
+        loss = 0
+        for x, y in data:
+            j = self.score(x, y, criteria)
+            loss += j.sum() / n
+        return loss
