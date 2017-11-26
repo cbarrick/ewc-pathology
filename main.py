b82c024cf6849c20e512e60b4e1a0afc54b2e669
Chris Barrick <cbarrick1@gmail.com>
2017-11-26 04:22:14 -0500

    Add cli argument for max epochs

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from models import AlexNet
 logger = logging.getLogger(__name__)
 
 
-def main(n_folds=5, batch_size=64, cuda=None):
+def main(n_folds=5, batch_size=64, epochs=100, cuda=None):
     net = AlexNet(2)
     opt = O.Adam(net.parameters())
     loss = N.CrossEntropyLoss()
@@ -43,7 +43,7 @@ def main(n_folds=5, batch_size=64, cuda=None):
         for task, loader in tasks.items():
             print(f'-------- Training on {task} --------')
             train, validation, _ = loader.load(f, batch_size=batch_size)
-            model.fit(train)
+            model.fit(train, max_epochs=epochs)
             model.consolidate(validation)
         for task, loader in tasks.items():
             print(f'-------- Scoring {task} --------')
@@ -57,6 +57,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the EWC experiment.')
     parser.add_argument('-k', '--n_folds', metavar='N', type=int, default=5, help='The number of cross-validation folds.')
     parser.add_argument('-b', '--batch_size', metavar='N', type=int, default=64, help='The batch size.')
+    parser.add_argument('-e', '--epochs', metavar='N', type=int, default=100, help='The maximum number of epochs per task.')
     parser.add_argument('-c', '--cuda', metavar='N', type=int, default=None, help='Use the Nth cuda device.')
     args = parser.parse_args()
-    main(args.n_folds, args.batch_size, args.cuda)
+    main(**args)
