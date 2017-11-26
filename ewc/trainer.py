e527ae1d615267c6e4358192bfb891e8f23dc67e
Chris Barrick <cbarrick1@gmail.com>
2017-11-25 21:33:37 -0500

    Move EWC logic from AlexNet to Trainer

--- ewc/trainer.py
+++ ewc/trainer.py
@@ -1,83 +1,116 @@
-import argparse
-from model import  alexnet
 import logging
-logging.basicConfig(level = logging.DEBUG)
-
-from datasets.nuclei import NucleiLoader
-from datasets.epi import EpitheliumLoader
-logger=logging.getLogger(__name__)
 
 import torch
-torch.manual_seed(1234)
+import torch.autograd as A
+import torch.nn as N
+import torch.nn.functional as F
+import torch.optim as O
 
-#parser loading arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", type=int)
-args = parser.parse_args()
-print(args)
-EPOCHS = args.e
+
+logging.basicConfig(level='DEBUG')
+logger = logging.getLogger(__name__)
 
 
 class Trainer:
+    def __init__(self, model, opt, loss):
+        '''Create an EWC trainer to train a model on multiple tasks.
 
-    def __init__(self, classes, batch_epoch = 1):
+        Args:
+            model: The model to train.
+            opt: The optimizer to step during training.
+            loss: The loss function to minimize.
+        '''
+        self.model = model
+        self.opt = opt
+        self._loss = loss
+        self._tasks = []
 
-        self.batch_epoch = batch_epoch
-        self.model = alexnet.AlexNet(classes);
+    def params(self):
+        '''Get the trainable paramaters from the optimizer.'''
+        return [p for group in self.opt.param_groups for p in group['params']]
 
+    def fisher_information(self, x, y):
+        '''Compute the Fisher information of the trainable parameters.
 
-    """
-    Trains model for specific task alos supports batch based iterations
-    name: The name of task to be given useful for logging purposes
-    train_x : Torch tensor to be accepted by the model as x arguments
-    train_y : Torch tensor to be accepted by the model as y arguments
+        Args:
+            x: A sample of inputs.
+            y: The associated labels.
+        '''
+        self.model.train()  # put the model in train mode, effects dropout layers etc.
+        self.opt.zero_grad()  # reset the gradients of the trainable variables.
+        x = A.Variable(x, volatile=True)
+        h = self.model(x)
+        l = F.log_softmax(h)[:, y]  # log-likelihood of true class
+        l.backward()
+        grads = (p.grad.detach().data for p in self.params())
+        fisher = [(g ** 2).mean(0) for g in grads]
+        return fisher
 
-    """
-    def train_on_task(self, name, train_x, train_y):
+    def consolidate(self, x, y, lambda=1):
+        '''Consolidate the weights of the current task.
 
-        logger.info("Training task {}".format(name))
-        for i in range(0, self.batch_epoch):
-            train_x = train_x.permute(0, 3, 1,2)
-            loss = self.model.partial_fit(train_x, train_y)
-            logger.debug("loss: {}".format(loss))
+        This adds an EWC regularization term to the loss function.
 
-        return train_x, train_y
+        Args:
+            x: A sample of inputs for the task.
+            y: The associated labels.
+        '''
+        params = [p.detach().data for p in self.params()]
+        fisher = self.fisher_information(x, y)
+        task = {
+            'params': params,
+            'fisher': fisher,
+            'lambda': lambda,
+        }
+        self._tasks.append(task)
 
-    """
-    Trains model for the consolidate phase of the ewc
-    name: The name of task to be given useful for logging purposes
-    train_x : Torch tensor to be accepted by the model as x arguments
-    train_y : Torch tensor to be accepted by the model as y arguments
+    def loss(self, h, y):
+        '''Compute the loss with EWC regularization.
 
-    """
-    def train_on_task_consolidate(self, name, train_x, train_y):
+        Args:
+            h: The hypothesis.
+            y: The labels.
+        '''
+        j = self._loss(h, y)
 
-        logger.info("Training task {}".format(name))
-        train_x = train_x.permute(0, 3, 1, 2)
-        self.model.consolidate(train_x, train_y)
+        # Return the normal loss if there are no consolidated tasks.
+        if len(self._tasks) == 0:
+            return j
 
+        # Add the ewc regularization for each consolidated task.
+        params = self.params()
+        for task in self._tasks:
+            ewc = ((p - t) ** 2 for t, p in zip(task['params'], params))
+            ewc = (f * e for f, e in zip(task['fisher'], ewc))
+            ewc = (l/2 * e for l, e in zip(task['lambda'], ewc)
+            j += sum(ewc)
+        return j
 
-    """
-    Trains model for the consolidate phase of the ewc
-    name: The name of task to be given useful for logging purposes
-    train_x : Torch tensor to be accepted by the model as x arguments
-    train_y : Torch tensor to be accepted by the model as y arguments
+    def predict(self, x):
+        '''Evaluate the model on some input batch.
 
-    """
-    def predict(self, test_x):
+        Args:
+            x: The input batch.
+        '''
+        self.model.eval()  # put the model in eval mode, effects dropout layers etc.
+        x = A.Variable(x, volatile=True)  # use volatile input to save memory when not training.
+        h = self.model(x)
+        return h
 
-        test_x = test_x.permute(0, 3, 1, 2)
-        return  self.model.predict(test_x)
+    def partial_fit(self, x, y):
+        '''Performs one step of the optimization.
 
-    """
-    Trains model for the  partial fit phase of the  ewc
-    name: The name of task to be given useful for logging purposes
-    train_x : Torch tensor to be accepted by the model as x arguments
-    train_y : Torch tensor to be accepted by the model as y arguments
-
-    """
-    def partial_fit(self, data_x1, data_y1):
-
-        first_data=self.train_on_task("MNIST", data_x1, data_y1)
-        logger.info("Task is trained ")
-        #woc=self.get_accuracy(self.model.predict(first_data[0]),data_y1)
+        Args:
+            x: The input batch.
+            y: The class labels.
+        '''
+        self.model.train()  # put the model in train mode, effects dropout layers etc.
+        self.opt.zero_grad()  # reset the gradients of the trainable variables.
+        x = A.Variable(x)
+        y = A.Variable(y)
+        h = self.model(x)
+        j = self.loss(h, y)
+        j.backward()
+        j = j.data[0]
+        self.opt.step()
+        return j
--- models/alexnet.py
+++ models/alexnet.py
@@ -10,10 +10,8 @@ logger=logging.getLogger(__name__)
 class AlexNet(N.Module):
 
     def __init__(self, num_classes=2):
-        super(AlexNet, self).__init__()
-        self.fisher={}
-        self.mean={}
-        self.__CONSOLIDATED=0
+        super().__init__()
+
         self.features = N.Sequential(
             N.Conv2d(3, 32, kernel_size=5, stride=1, padding=2),
             N.MaxPool2d(kernel_size=3, stride=2, padding=1),
@@ -25,6 +23,7 @@ class AlexNet(N.Module):
             N.ReLU(inplace=True),
             N.AvgPool2d(kernel_size=3, stride=2, padding=1),
         )
+
         self.classifier = N.Sequential(
             N.Linear(64*8*8, 64),
             N.Dropout(),
@@ -33,51 +32,9 @@ class AlexNet(N.Module):
             N.Dropout(),
             N.ReLU(inplace=True),
         )
-        logger.debug("CNN Initialised.")
-        self.lossfn=N.CrossEntropyLoss()
-        self.optimizer=O.Adam(self.parameters())
-        logger.debug("Loss function:{} Optimizer:{}".format(self.lossfn,self.optimizer))
 
     def forward(self, x):
         x = self.features(x)
-        #print(x)
-        x = x.view(x.size(0), 256 * 5 * 5)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
-
-    def partial_fit(self,X,y):
-        X=A.Variable(X)
-        y=A.Variable(y)
-        out=self.forward(X)
-        loss=self.lossfn(out,y.long())
-        logger.info("Loss: {}".format(loss.data[0]))
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        return loss.data[0]
-
-    def predict(self,x):
-        x=A.Variable(x)
-        out=F.softmax(self.forward(x))
-        return torch.max(out.data,1)[1].numpy()
-
-    def get_ewc_loss(self,l=15):
-        losses=[]
-        for name,param in self.named_parameters():
-            mean=A.Variable(self.mean[name])
-            fisher=A.Variable(self.fisher[name])
-            loss=fisher*((param-mean)**2)
-            losses.append(loss.sum())
-        return (l/2)*sum(losses)
-
-    def consolidate(self,x,y):
-        logger.debug("Consolidating weights.")
-        x=A.Variable(x)
-        y=A.Variable(y).long()
-        size=x.size()[0]
-        log_liklihood=F.log_softmax(self.forward(x))[range(size),y.data]
-        derivatives=A.grad(log_liklihood.sum(),self.parameters())
-        for i,(name,param) in enumerate(self.named_parameters()):
-            self.fisher[name]=(derivatives[i].data.clone()**2)/size
-            self.mean[name]=param.data.clone()
-        self.__CONSOLIDATED=1
