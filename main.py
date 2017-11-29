ca65d57077009478b97b84fd7cd3e932dd2fef54
Chris Barrick <cbarrick1@gmail.com>
2017-11-28 23:47:40 -0500

    Add --patience argument

--- main.py
+++ main.py
@@ -23,9 +23,10 @@ logger = logging.getLogger()
 def main(
     data_size=10000,
     n_folds=5,
-    batch_size=64,
     epochs=100,
+    patience=10,
     ewc_strength=1,
+    batch_size=128,
     cuda=None,
     dry_run=False,
     log_level='DEBUG',
@@ -65,7 +66,7 @@ def main(
         for task, loader in tasks.items():
             print(f'-------- Training on {task} --------')
             train, validation, _ = loader.load(f)
-            model.fit(train, validation, max_epochs=epochs, **data_args)
+            model.fit(train, validation, max_epochs=epochs, patience=patience, **data_args)
             model.consolidate(validation, alpha=ewc_strength, **data_args)
             print()
 
@@ -85,9 +86,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the EWC experiment.')
     parser.add_argument('-n', '--data-size', metavar='N', type=int, default=10000, help='the number of training samples is a function of N')
     parser.add_argument('-k', '--n-folds', metavar='N', type=int, default=5, help='the number of cross-validation folds')
-    parser.add_argument('-b', '--batch-size', metavar='N', type=int, default=64, help='the batch size')
     parser.add_argument('-e', '--epochs', metavar='N', type=int, default=100, help='the maximum number of epochs per task')
+    parser.add_argument('-p', '--patience', metavar='N', type=int, default=10, help='higher patience may help avoid local minima')
     parser.add_argument('-w', '--ewc-strength', metavar='N', type=float, default=1, help='the regularization strength of EWC')
+    parser.add_argument('-b', '--batch-size', metavar='N', type=int, default=128, help='the batch size')
     parser.add_argument('-c', '--cuda', metavar='N', type=int, default=None, help='use the Nth cuda device')
     parser.add_argument('-d', '--dry-run', action='store_true', help='do a dry run to check for errors')
     parser.add_argument('-l', '--log-level', help='set the log level')
